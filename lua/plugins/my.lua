e27255c525e7eb8f32162c185bf14e6429b271ff
can <certen@gmail.com>
2024-04-04 22:20:03 +0100

    color

--- lua/plugins/my.lua
+++ lua/plugins/my.lua
@@ -1,8 +1,16 @@
 return {
+  { "shaunsingh/nord.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+
   {
     "LazyVim/LazyVim",
     opts = {
+      --    colorscheme = "nord",
+      -- colorscheme = "gruvbox",
       colorscheme = "catppuccin-macchiato",
+
+      -- colorscheme = "tokyonight",
+      -- colorscheme = "onedark",
     },
   },
 }
