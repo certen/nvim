b89c8582176e747793bd46b422bf2dfc17ed0e0b
can <certen@gmail.com>
2024-04-07 21:19:06 +0100

    new mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+map("n", "<C-d>", "<C-d>zz", { desc = "Center down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Center up" })
+map("n", "<C-b>", "<C-b>zz", { desc = "Center back" })
+map("n", "<C-f>", "<C-f>zz", { desc = "Center forward" })
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center down" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center up" })
-vim.keymap.set("n", "<C-b>", "<C-b>zz", { desc = "Center back" })
-vim.keymap.set("n", "<C-f>", "<C-f>zz", { desc = "Center forward" })
+map("n", "<C-A>", "$", { desc = "Go to end" })
+map("n", "<C-I>", "_", { desc = "Go to beginning" })
